4a6ea55af2786c583421f5856e63353e8a8b9df0
amreshtiwi <ammr64@outlook.com>
2024-01-21 22:22:55 +0200

    finish the summary page

--- src/App.tsx
+++ src/App.tsx
@@ -3,17 +3,18 @@ import './app.scss'
 import { Home } from './components/home/Home'
 import { Navbar } from './components/navbar/Navbar'
 import { Parallax } from './components/parallax/Parallax'
+import { Summary } from './components/summary/Summary'
 
 function App() {
 
   return (
     <div>
       <section id='Home'>
-        <Navbar/>
-        <Home/>
-        </section>
-      <section id='Experince'><Parallax/></section>
-      <section id='Education'>education</section>
+        <Navbar />
+        <Home />
+      </section>
+      <section id='Experince'><Parallax /></section>
+      <section id='Education'><Summary /></section>
       <section id='Projects'>projects</section>
       <section id='Skills'>skills</section>
       <section id='Hobbie'>hobies</section>
