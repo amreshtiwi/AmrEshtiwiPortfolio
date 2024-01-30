bcda12d75bf71543361f9acbde84a1af2a4451e8
amreshtiwi <ammr64@outlook.com>
2024-01-30 14:51:59 +0200

    create the the experince and eduaction sections without animation

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 // import './App.css'
 import './app.scss'
+import { Experince } from './components/experince/Experince'
 import { Home } from './components/home/Home'
 import { Navbar } from './components/navbar/Navbar'
 import { Parallax } from './components/parallax/Parallax'
 import { Summary } from './components/summary/Summary'
+import { Education } from './components/education/Education'
 
 function App() {
 
@@ -15,8 +17,8 @@ function App() {
       </section>
       <section id='Experince'><Parallax /></section>
       <section id='Education'><Summary /></section>
-      <section id='Projects'>projects</section>
-      <section id='Skills'>skills</section>
+      <section id='Projects'><Experince/></section>
+      <section id='Skills'><Education/></section>
       <section id='Hobbie'>hobies</section>
       <section id='Contact'>contact me</section>
     </div>
---
+++ src/components/experince/company/Company.tsx
@@ -0,0 +1,38 @@
+import './company.scss'
+
+export const Company = () => {
+    return (
+    
+            <div className='company-container'>
+                <div className="company">
+                    <h1 className='position'>Frontend Developer Internship</h1>
+                    <span>
+                        <h2>Harri</h2>
+                        <p>09/2022 - 12/2022</p>
+                        <p>Ramallah, Palestine</p>
+                    </span>
+                    {/* <ul>
+                        <li>Developed experience in the front-end field by creating responsive web pages, implementing routing, and handling APIs.</li>
+                        <li>Gained proficiency in core web development concepts, including HTML, CSS, and JavaScript.</li>
+                        <li>Mastered popular libraries and frameworks such as Bootstrap, React, and Angular, enabling the creation of dynamic web applications.</li>
+                        <li>Acquired fundamental knowledge of version control using Git, facilitating collaborative software development projects.</li>
+                        <li>Achieved developing and building a web application called Where in the World in four ways (HTML, CSS, and JS, Bootstrap, React, and Angular). It was built successfully to allow users to access the information of the countries.</li>
+                    </ul> */}
+
+                    
+                    {/* <a href='https://github.com/amreshtiwi/Where-in-the-world---traning_content'>Where in the world</a> */}
+                 
+                </div>
+
+
+                <img src="/wherein.png" alt="wherein" />
+                <ul>
+                        <li>Developed experience in the front-end field by creating responsive web pages, implementing routing, and handling APIs.</li>
+                        <li>Gained proficiency in core web development concepts, including HTML, CSS, and JavaScript.</li>
+                        <li>Mastered popular libraries and frameworks such as Bootstrap, React, and Angular, enabling the creation of dynamic web applications.</li>
+                        <li>Acquired fundamental knowledge of version control using Git, facilitating collaborative software development projects.</li>
+                        <li>Achieved developing and building a web application called Where in the World in four ways (HTML, CSS, and JS, Bootstrap, React, and Angular). It was built successfully to allow users to access the information of the countries.</li>
+                    </ul>
+            </div>
+    )
+}
